c062cd4c9c2bf580053e405b87e5634097c4f37e
micro <micro@NB-Basti>
2025-11-17 16:59:41 +0100

    version after lecture on monday

--- stepper/Core/Inc/Stepper_implementation/my_stepper.h
+++ stepper/Core/Inc/Stepper_implementation/my_stepper.h
@@ -6,7 +6,12 @@ static void* StepLibraryMalloc( unsigned int size );
 static void StepLibraryFree( const void* const ptr );
 static int StepDriverSpiTransfer( void* pIO, char* pRX, const char* pTX, unsigned int length );
 
-// own functions:
-static int reset_stepper(void);
+static void StepDriverReset(void *pGPO, const int ena);
+static void StepLibraryDelay(unsigned int ms);
+// static int StepTimerAsync(void *pPWM, int dir, unsigned int numPulses, void(*doneClb)(L6474_Handle_t), L6474_Handle_t h);
+// static int StepTimerCancelAsync(void *pPWM);
+
+// own functions - brauche ich dir ueberhaupt noch?:
+void reset_stepper(void);
 
 #endif
--- stepper/Core/Src/Stepper_implementation/my_stepper.c
+++ stepper/Core/Src/Stepper_implementation/my_stepper.c
@@ -5,51 +5,96 @@
 #include "FreeRTOSConfig.h"
 #include "main.h"
 #include "stdint.h"
+#include "stdlib.h"
 
+// in main.c definiert
+extern SPI_HandleTypeDef hspi1;
+
+void reset_stepper(void)
+{
+	L6474_BaseParameter_t base_parameter;
+	L6474_SetBaseParameter(&base_parameter);
+
+	// from LibL6474 library documentation:
+	// pass all function pointers required by the stepper library
+	// to a separate platform abstraction structure
+	L6474x_Platform_t p;
+	p.malloc     = StepLibraryMalloc;
+	p.free       = StepLibraryFree;
+	p.transfer   = StepDriverSpiTransfer;
+	p.reset      = StepDriverReset;
+	p.sleep      = StepLibraryDelay;
+
+	// auskommentiert, da Flag in LibL6474Config.h Header gesetzt wurde
+	// p.stepAsync  = StepTimerAsync;
+	// p.cancelStep = StepTimerCancelAsync;
+
+
+	// now create the handle
+	L6474_Handle_t h = L6474_CreateInstance(&p, NULL, NULL, NULL);
+}
 
 // from LibL6474 library documentation
 static void* StepLibraryMalloc( unsigned int size )
 {
-     return malloc(size);
+	// size	number of bytes requested by the memory allocation request
+	return malloc(size);
 }
 
 // from LibL6474 library documentation
 static void StepLibraryFree( const void* const ptr )
 {
-     free((void*)ptr);
+	free((void*)ptr);
 }
 
 // from LibL6474 library documentation
 static int StepDriverSpiTransfer( void* pIO, char* pRX, const char* pTX, unsigned int length )
 {
 	// byte based access, so keep in mind that only single byte transfers are performed!
+	// siehe stm32f7xx_hal_def.h Z. 38
+	HAL_StatusTypeDef errorcode = HAL_OK;
+
+	// bringe Chips Select low to transfer data
+	HAL_GPIO_WritePin(STEP_SPI_CS_GPIO_Port, STEP_SPI_CS_Pin, 0);
 	for ( unsigned int i = 0; i < length; i++ )
 	{
-	//TODO
+		// TransmitReceive, damit weder Transmit Byte noch Receive Byte verworfen wird
+		// 1 Byte versenden und 1 Byte empfangen, HAL_MAX_DELAY aus stm32f7xx_hal_def.h Z. 61
+		errorcode = HAL_SPI_TransmitReceive(&hspi1, (uint8_t*) pTX, (uint8_t*) pRX, 1, HAL_MAX_DELAY);
+		if (errorcode != HAL_OK)
+		{
+			return -1;
+		}
+
 	}
+	// bringe Chips Select hight to end data transfer
+	HAL_GPIO_WritePin(STEP_SPI_CS_GPIO_Port, STEP_SPI_CS_Pin, 1);
+
 	return 0;
 }
 
-//TODO
-
-static int reset_stepper(void)
+static void StepDriverReset(void *pGPO, const int ena)
 {
-	L6474_BaseParameter_t base_parameter;
-	L6474_SetBaseParameter(&base_parameter);
+
 }
-// TODO
 
-// from LibL6474 library documentation:
-// pass all function pointers required by the stepper library
-// to a separate platform abstraction structure
-L6474x_Platform_t p;
-p.malloc     = StepLibraryMalloc;
-p.free       = StepLibraryFree;
-p.transfer   = StepDriverSpiTransfer;
-p.reset      = StepDriverReset;
-p.sleep      = StepLibraryDelay;
-p.stepAsync  = StepTimerAsync;
-p.cancelStep = StepTimerCancelAsync;
+static void StepLibraryDelay(unsigned int ms)
+{
 
-// now create the handle
-L6474_Handle_t h = L6474_CreateInstance(&p, null, null, null);
+}
+
+static int StepTimerAsync(void *pPWM, int dir, unsigned int numPulses, void(*doneClb)(L6474_Handle_t), L6474_Handle_t h)
+{
+	// TODO for Async Movement
+
+	// return 0 just for debugging
+	return 0;
+}
+
+static int StepTimerCancelAsync(void *pPWM)
+{
+	// TODO for Async Movement
+
+	// return 0 just for debugging
+	return 0;
+}
