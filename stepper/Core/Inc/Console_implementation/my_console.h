189364dc2fbbf95ef3cf2f3402ba9aa2f45b586e
micro <micro@NB-Basti>
2025-11-17 10:02:42 +0100

    move headers in include folder

---
+++ stepper/Core/Inc/Console_implementation/my_console.h
@@ -0,0 +1,6 @@
+#ifndef MY_CONSOLE_H
+#define MY_CONSOLE_H
+
+void MyConsole_Init(void);
+
+#endif
---
+++ stepper/Core/Inc/Spindle_implementation/my_spindle.h
@@ -0,0 +1,20 @@
+/*
+ * spindle.h
+ *
+ *  Created on: Oct 27, 2025
+ *      Author: Basti
+ */
+
+#ifndef MY_SPINDLE_H
+#define MY_SPINDLE_H
+
+
+#include "Spindle.h"
+//Initial Configurations
+void init_Spindle(void);
+//notwendige Harware Funktionen, die an die Lib Spindle uebergeben werden muessen
+void SPINDLE_SetDirection(SpindleHandle_t h, void* context, int backward);
+void SPINDLE_SetDutyCycle(SpindleHandle_t h, void* context, float dutyCycle);
+void SPINDLE_EnaPWM(SpindleHandle_t h, void* context, int ena);
+
+#endif
